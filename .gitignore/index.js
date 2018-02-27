a4c3539b42d9e3db9622cbb7ce2d799466b529e1
TitanZ4One <36897295+TitanZ4One@users.noreply.github.com>
2018-02-27 22:10:04 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,7 +10,18 @@ bot.on("ready", function() {
 bot.login("NDE4MDk4OTcyMjA5NTc3OTg0.DXcoog.Tbiihstrvr4vABVZahi7LG-W99g");
 
 bot.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === 'Ping') {
     message.channel.sendMessage('pong');
   }
 });
+bot.on('message', message => {
+    if (message.content === 'Salut') {
+    message.channel.sendMessage('salut moi cest Jeff Tuche Heureux dte rencontrer mon ptit gars');
+  }
+});
+bot.on('message', message => {
+    if (message.content === 'Sa va') {
+    message.channel.sendMessage('bien et toi mon chti gars');
+  }
+});
+
